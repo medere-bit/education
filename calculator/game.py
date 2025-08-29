e1b87e4a66f4ad84f489766c0b680a20cc316541
Vitaliy Yakovlev <vintyak@gmail.com>
2025-08-29 18:23:37 +0300

    Add numbers generator

---
+++ calculator/game.py
@@ -0,0 +1,82 @@
+from tkinter import *
+
+import time
+import random
+
+tk = Tk()
+
+tk.title('Game')
+
+tk.resizable(0,0)
+
+tk.wm_attributes('topmost', 1)
+
+canvas = Canvas(tk, width=500, height=400, highlightthickness=0)
+
+canvas.pack()
+
+tk.update()
+
+class Ball:
+    def __init__(self, canvas, paddle, score, color):
+
+        self.canvas = canvas
+        self.paddle = paddle
+        self.score = score
+
+        self.id = canvas.create_oval(10,10, 25, 25, fill=color)
+
+        self.canvas.move(self.id, 245, 100)
+
+        starts = [-2, -1, 1, 2]
+
+        random.shuffle(starts)
+
+        self.x = starts[0]
+
+        self.y = -2
+
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+
+        self.hit_bottom = False
+
+        def hit_paddle(self, pos):
+
+            paddle_pos = self.canvas.coords(self.paddle.id)
+
+            if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+                if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+
+                    self.score.hit()
+
+                    return True
+
+            return False
+        def draw(self):
+
+            self.canvas.move(self.id, self.x, self.y)
+
+            pos = self.canvas.coords(self.id)
+
+            if pos[0]  <= 0:
+
+            self.y = 2
+
+        if pos[3] >= self.canvas_height:
+
+            self.hit.bottom = True
+
+            canvas.create_text(250, 120, text='К сожалению вы проиграли, попробуйте сыграть ещё раз', font=('Courier', 30), fill='red')
+
+        if self.hit_paddle(pos) == True:
+
+            self.y = -2
+
+        if pos[0] <= 0:
+
+            self.x = 2
+
+        if pos[2] >= self.canvas_width:
+
+            self.x = -2
